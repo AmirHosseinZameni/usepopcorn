09d292dd93fc01df61e15588777de14c0c59ee43
AmirHosseinZameni <amirhossein.zameni@gmail.com>
2026-01-12 20:43:08 +0330

    props as an API

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating/>
+    <StarRating color='red' size={32}/>
   </React.StrictMode>
 );
 
